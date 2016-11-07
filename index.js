d89342a5ee4b5d9035af169eeb054f10b054798b
Allon Segall <Segal500@gmail.com>
2016-11-07 15:12:09 +0200

    Bkeeper day 2

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-document.getElementById('outerdiv').style.backgroundColor = "aqua";
-
 function myFunction(){
-document.getElementById('t1').innerHTML = "read more books";
+  document.getElementById('t1').innerHTML = "read more books";
 }
+
+var funClick = function(){
+  if (document.getElementById("myPassword").value.length < 6) {
+    document.getElementById("msg").innerHTML = "password too short";
+    document.getElementById("msg").style.backgroundColor = "red";
+  } if (document.getElementById("myPassword").value !== document.getElementById("rePass").value) {
+    document.getElementById("msg").innerHTML = "password must match";
+    document.getElementById("msg").style.backgroundColor = "red";
+  } else {
+    document.getElementById("msg").innerHTML = "password saved";
+    document.getElementById("msg").style.backgroundColor = "green";
+  }
+};
+
+document.getElementById("btnSU").onclick=funClick;
